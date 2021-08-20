cdda1e335b433f510d13d0eea7e7583c22d07f2c
osmarloureiro <osmarloureiro@gmail.com>
2021-08-20 12:26:53 +0900

    curso javascript2021

--- dados_2.js
+++ dados_2.js
@@ -2,3 +2,11 @@
 let preco = 19.90;
 let desconto = 0.4;
 console.log(preco * (1 - desconto)); // colocando () vai executar primeiro
+
+let nome = "caderno"; // string (texto)-> sequencia de simbolos
+let categoria = "papelaria";
+
+console.log("produto: " + nome
+         + ", categoria: " + categoria
+         + ", preço: " + preco
+         + ", desconto " + desconto);
