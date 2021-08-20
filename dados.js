92f54fcc00a4725e5283d2f97be7137e73638ad2
osmarloureiro <osmarloureiro@gmail.com>
2021-08-20 12:18:20 +0900

    curso javascript2021

---
+++ dados.js
@@ -0,0 +1,23 @@
+/*
+var nome = "caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+*/
+
+let nome = "caneta";
+let quantidade = 10;
+let preco = 6.4;
+let imposto = 1.5;
+let precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade); // valor literal
+console.log(preco); //separador de casas decimais usa o ponto.
+console.log(imposto);
+console.log(precoFinal);
+
+// pode mudar o valor da variavel durante o processo.
+var nome = "caneta bic";
+console.log(nome);
---
+++ dados_2.js
@@ -0,0 +1,4 @@
+// vou usar let como atribuicao de variavel
+let preco = 19.90;
+let desconto = 0.4;
+console.log(preco * (1 - desconto)); // colocando () vai executar primeiro
